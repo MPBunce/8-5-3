ae22d837e1564120439896ff67cdb85e770a2c60
Matthew Bunce <mbunce@uoguelph.ca>
2023-05-20 21:08:48 -0400

    setup creat user royte

--- backend/controllers/userControllers.ts
+++ backend/controllers/userControllers.ts
@@ -1,5 +1,7 @@
 const asyncHandler = require('express-async-handler');
+const bcrypt = require('bcryptjs');
 
+import User from "../models/userModel";
 
 //@desc     Auth user/set token
 //route     POST /api/users/auth
@@ -14,6 +16,31 @@ const authUser = asyncHandler (async (req: any, res: any) => {
 //route     POST /api/users
 //@access   public
 const registerUser = asyncHandler (async (req: any, res: any) => {
+
+    const { name, email, password } = req.body
+
+    const userExists = await User.findOne({ email: email})
+    if (userExists){
+        res.status(400);
+        throw new Error('User Already Exists');
+    };
+
+    const user = await User.create({
+        name,
+        email,
+        password
+    });
+
+    if(user){
+        res.status(201).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+        })
+    } else {
+        res.status(400);
+        throw new Error(`Invalid user data`);
+    };
 
     res.status(200).json({ message: 'Register User'});
 
--- backend/server.ts
+++ backend/server.ts
@@ -3,12 +3,19 @@ const express = require('express');
 const dotenv = require('dotenv');
 import userRoutes from './routes/userRoutes'
 import {notFound, errorHandler} from './middleware/errorMiddleware'
+import connectDB from './config/db';
 
 //Initiliez Apps
 dotenv.config();
+
 const port = process.env.PORT || 5000;
+
+connectDB();
+
 const app = express();
 
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
 
 app.use('/api/users', userRoutes);
 app.get( '/', (req: any, res: any) => res.send('server is running') );

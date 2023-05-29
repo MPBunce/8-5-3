9c7eb7a334a72d3b1a779f52e9c727b71ea79e79
Matthew Bunce <mbunce@uoguelph.ca>
2023-05-28 21:11:07 -0400

    fixed

--- frontend/app/_layout.tsx
+++ frontend/app/_layout.tsx
@@ -3,33 +3,41 @@ import React from 'react'
 import { Stack } from 'expo-router'
 import { useRouter } from 'expo-router'
 import store from '../store'
-import Provider from 'react-redux'
+import { Provider } from 'react-redux';
 
 const StackLayout = () => {
 
   const router = useRouter();
 
   return (
-    <Stack>
-      <Stack.Screen name='index' options={{
-          headerTitle: 'Login',
-          headerShown: true
-      }}/>
-      <Stack.Screen name='Register' options={{
-          headerTitle: 'Register',
-          headerShown: true,
-          // headerRight: () => <Button title='about' onPress={
-          //   () => {
-          //     router.push('About')
-          //   }
-          // }/> 
-      }}/>
+    <Provider store={store}>
 
-      <Stack.Screen name='About' options={{ presentation: 'modal'}}/>
+      <Stack>
 
-      <Stack.Screen name="(tabs)" options={{ headerShown: false}}/>
+        <Stack.Screen name='index' options={{
+            headerTitle: 'Login',
+            headerShown: true
+        }}/>
+        <Stack.Screen name='Register' options={{
+            headerTitle: 'Register',
+            headerShown: true,
+            // headerRight: () => <Button title='about' onPress={
+            //   () => {
+            //     router.push('About')
+            //   }
+            // }/> 
+        }}/>
 
-    </Stack>
+        <Stack.Screen name='About' options={{ presentation: 'modal'}}/>
+
+        <Stack.Screen name="(tabs)" options={{ headerShown: false}}/>
+
+
+      </Stack>
+
+
+    </Provider>
+
   )
 }
 
